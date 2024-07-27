28cf6581862140ad0debec67fbf9204f90b4bc59
lhq <3289468135@qq.com>
2024-07-27 16:25:18 +0800

    test-03

---
+++ src/main/java/com/example/testgit/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.testgit;
+
+/**
+ * @author LHQ
+ * @description :
+ */
+public class HelloController {
+    public String sayHello1() {
+        System.out.println("qqwqe");
+        return "Hello World!";
+    }
+}
